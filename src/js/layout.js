6877c7c1b7e090f576f3356b84b41dfda45107e7
hernanjkd <hernanjkd@gmail.com>
2020-03-26 17:33:41 -0400

    update

--- src/js/layout.js
+++ src/js/layout.js
@@ -6,9 +6,6 @@ import { Home } from "./views/home";
 import { SignUp } from "./views/signup";
 import injectContext from "./store/appContext";
 
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
-
 //create your first component
 export const Layout = () => {
 	//the basename is used when your project is published in a subdirectory and not in the root of the domain
